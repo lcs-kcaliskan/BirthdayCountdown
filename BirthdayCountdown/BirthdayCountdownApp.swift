18f6a1131020117a8c3ba03bf08881766a2d08cb
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-20 09:19:33 -0500

    Initial Commit

---
+++ BirthdayCountdown/BirthdayCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayCountdownApp.swift
+//  BirthdayCountdown
+//
+//  Created by Ahmet Kaan Caliskan on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
