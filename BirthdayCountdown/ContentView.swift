db9f2c11f091cd761aad052a0e3ffdb1d0c7a945
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-21 12:09:37 -0500

    added a guard but doesn't work

--- BirthdayCountdown/ContentView.swift
+++ BirthdayCountdown/ContentView.swift
@@ -9,32 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // MARK: Stored properties
     let mytime = Date()
-  
-   @State private var givenInputDD = ""
+    
+    @State private var givenInputDD = ""
     @State private var givenInputMM = ""
     @State private var feedback = ""
+    @State private var daysUntilBirthday = ""
 
-   
+    // MARK: Computed properties
     var body: some View {
-       
+        
         VStack {
             HStack{
-            Text("Enter your birth month(1-12):")
+                Text("Enter your birth month(1-12):")
                 TextField("Type", text: $givenInputMM)
-        }
+            }
             HStack{
-            Text("Enter your birth date(1-31):")
+                Text("Enter your birth date(1-31):")
                 TextField("Type", text: $givenInputDD)
-        }
+            }
             Text("\(feedback)")
             Text("Your birthday is (day/month): \(givenInputDD)/\(givenInputMM)")
             
-           Text("Placeholder")
-        
+            Text("Placeholder")
+            
             
         }
         .padding()
+    }
+    
+    // MARK: Functions
+    func checkTextFields() {
+        guard let selectedMonth =  Int(givenInputMM) else {
+            feedback = "Please provide an integer."
+            return
+        }
+        guard let selectedDay =  Int(givenInputDD) else {
+            feedback = "Please provide an integer."
+            return
+        }
+        
     }
 }
 
