0d4389cf745fff57386f4a96f0f3ee0d7cb997ba
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-22 11:25:13 -0500

    The preview things waas wrong because it was inside the structure now it's proper

--- BirthdayCountdown/ContentView.swift
+++ BirthdayCountdown/ContentView.swift
@@ -53,9 +53,8 @@ struct ContentView: View {
         }
     }
     
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-    }
+}
+
+#Preview {
+    ContentView()
 }
