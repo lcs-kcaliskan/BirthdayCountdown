20d9f273d50cb00354213ba10abde601bd30bdf0
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-22 12:22:55 -0500

    Switched to a completely different way there's a date picker now so no need for any of the studd I spent 2 classes on

--- BirthdayCountdown/ContentView.swift
+++ BirthdayCountdown/ContentView.swift
@@ -10,27 +10,20 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: Stored properties
-    let mytime = Date()
-    
-    @State private var givenInputDD = ""
-    @State private var givenInputMM = ""
+
     @State private var feedback: String? = nil
     @State private var daysUntilBirthday: Int? = nil
-    
+    @State private var date = Date()
+    @State private var birthday = Date()
     // MARK: Computed properties
     var body: some View {
         
         VStack {
-            HStack{
-                Text("Enter your birth month(1-12):")
-                TextField("Type", text: $givenInputMM)
-            }
-            HStack{
-                Text("Enter your birth date(1-31):")
-                TextField("Type", text: $givenInputDD)
-            }
-            Text("Your birthday is (day/month): \(givenInputDD)/\(givenInputMM)")
-            
+            Text("Choose your birthday")
+            DatePicker("Choose your birthday", selection: $birthday, displayedComponents: .date)
+            .labelsHidden()
+            .padding(.horizontal)
+                       
             Button("Calculate Days Until Next Birthday") {
                 calculateDaysUntilNextBirthday()
             }
@@ -51,18 +44,12 @@ struct ContentView: View {
     
     // MARK: Functions
     func calculateDaysUntilNextBirthday() {
-        feedback = nil
-        daysUntilBirthday = nil
+       
+        }
         
-        guard let month = Int(givenInputMM), month >= 1, month <= 12 else {
-            feedback = "Please enter a valid month (1-12)."
-            return
-        }
-        guard let day = Int(givenInputDD), day >= 1, day <= 31 else {
-            feedback = "Please enter a valid day between 1-31."
-            return
-        }
     }
+    
+    
 }
 #Preview {
     ContentView()
